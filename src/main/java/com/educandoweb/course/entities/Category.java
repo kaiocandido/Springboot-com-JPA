97211fc1f33d3fdb738e1a697c56239d7189d589
adm <kaio.candf@gmail.com>
2025-11-06 16:06:54 -0300

    created categories full

---
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -0,0 +1,64 @@
+package com.educandoweb.course.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "tb_categories")
+public class Category  implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+
+    public Category(){
+
+    }
+
+    public Category(String name, Integer id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+
+}
---
+++ src/main/java/com/educandoweb/course/services/CategoryService.java
@@ -0,0 +1,29 @@
+package com.educandoweb.course.services;
+
+
+import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.repositories.CategoryRepository;
+import com.educandoweb.course.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+//@Component
+//@Repository
+@Service
+public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> findAll(){
+        return categoryRepository.findAll();
+    }
+
+    public Category findById(Integer id){
+        Optional<Category> obj  = categoryRepository.findById(id);
+        return obj.get();
+    }
+}
