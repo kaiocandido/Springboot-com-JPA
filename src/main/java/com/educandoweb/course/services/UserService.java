73e3af98c19f0b44257d4b0ec10fc09955e70f44
adm <kaio.candf@gmail.com>
2025-11-11 22:13:26 -0300

    criado metodo insert user

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -23,4 +23,11 @@ public class UserService {
         Optional<User> obj  = userRepository.findById(id);
         return obj.get();
     }
+
+    public User insert(User obj){
+        return userRepository.save(obj);
+    }
+
+
+
 }
